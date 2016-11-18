344e8389b0151c7c9c8db3422c1cf807ab2b0b90
BWTFordeman <cbt_97@hotmail.com>
2016-11-18 14:38:09 +0100

    Fixed DoorInteraction. +some script stuff

--- Assets/Scripts/doorInteraction.cs
+++ Assets/Scripts/doorInteraction.cs
@@ -2,20 +2,40 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class doorInteraction : MonoBehaviour {
-
+public class DoorInteraction : MonoBehaviour {
+    bool inTrigger = false;
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.E))      //If pressed -goto next room:
+        if (inTrigger)
         {
-            SceneManager.LoadSceneAsync("level2");
-            //SceneManager.LoadScene("level2");
+            if (Input.GetKeyDown(KeyCode.E))      //If pressed -goto next room:
+            {
+                SceneManager.LoadSceneAsync("level2");
+                Debug.Log("Try to open door");
+            }
         }
-	}
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            inTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            inTrigger = false;
+        }
+    }
 }
