04580eddc7130bada1e9d414ec438cc40f2a7db8
Halvor Bakken Smedås <stektpotet@gmail.com>
2016-11-18 14:00:13 +0100

    Added player controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class PlayerController : MonoBehaviour
+{
+	//Properties
+	bool facingRight = true;
+	public bool grounded = false;
+	public float maxSpeed;
+	public float jumpForce;
+
+	//Components
+	Rigidbody2D body;
+	Animator anim;
+
+	//Other
+	public LayerMask groundMask;
+	public Transform groundPoint;
+	public float groundRadius = 0.2f;
+
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+
+
+	void Update()
+	{
+		Movement();
+	}
+
+	void FixedUpdate()
+	{
+		FixedMovement();
+	}
+
+	void Movement()
+	{
+		
+		if(Input.GetKeyDown(KeyCode.Space) && grounded)
+		{
+			body.AddForce(Vector2.up * jumpForce);
+		}
+	}
+
+	void FixedMovement()
+	{
+		grounded = Physics2D.OverlapCircle(groundPoint.position, groundRadius, groundMask);
+
+		float move = Input.GetAxis("Horizontal");
+
+		anim.SetFloat("Speed", Mathf.Abs(move));
+
+		body.velocity = new Vector2(move * maxSpeed, body.velocity.y);
+
+		if(move < 0 && facingRight || move > 0 && !facingRight)
+		{
+			Flip();
+		}
+	}
+    void Flip()
+	{
+		facingRight = !facingRight;
+		GetComponent<SpriteRenderer>().flipX = !facingRight;
+	}
+}
