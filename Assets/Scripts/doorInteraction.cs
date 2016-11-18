788af91d899611029f4f2833e639dfeaa71885f7
BWTFordeman <cbt_97@hotmail.com>
2016-11-18 14:08:21 +0100

    ADded doorInteraction

---
+++ Assets/Scripts/doorInteraction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class doorInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.E))      //If pressed -goto next room:
+        {
+            SceneManager.LoadSceneAsync("level2");
+            //SceneManager.LoadScene("level2");
+        }
+	}
+}
