ba46f7072e276ea54d20a7ee565f49b9afe49213
Halvor <stektpotet@gmail.com>
2016-11-23 21:34:46 +0100

    Finalized Alphabrush 1.0
    Blending of textures within unity!

--- Assets/Editor/AlphaBrushWindow.cs
+++ Assets/Editor/AlphaBrushWindow.cs
@@ -1,122 +1,66 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
 using System.Linq;
 using System.Collections.Generic;
 
-public class AlphaBrushWindow : EditorWindow
+public class AlphaBrushWizard : ScriptableWizard
 {
+	string[] a = { "red", "green", "blue", "alpha" };
 
-	const int xCount = 6;
-	int color = 0, brush = 0;
-	string[] a = { "red", "blue", "green", "alpha" };
-	private Texture2D[] m_brushes;
-	float radius = 3.0f;
+	PaintedMask mask;
 
-	Texture2D[] brushes
+	void OnEnable()
 	{
-		get
+		minSize = new Vector2(210f, 200f);
+	}
+
+	void OnDisable()
+	{
+		mask.isPainting = false;
+	}
+
+	public static void CreateWizard(PaintedMask mask)
+	{
+		AlphaBrushWizard wizard = DisplayWizard<AlphaBrushWizard>("AlphaBrush", "Apply");
+		wizard.mask = mask;
+	}
+	void OnWizardCreate()
+	{ }
+	//Texture2D[] Brushes()
+	//{
+	//	List<Texture2D> t = new List<Texture2D>();
+	//	t.AddRange(Resources.LoadAll<Texture2D>("Gizmos"));
+	//	t.AddRange(Resources.FindObjectsOfTypeAll<Texture2D>().Where( x => x.format == TextureFormat.Alpha8));
+	//	return t.ToArray();
+	//}
+
+	protected override bool DrawWizardGUI()
+	{
+		EditorGUI.BeginChangeCheck();
+		EditorGUILayout.BeginHorizontal();
+		PaintedMask.color = GUILayout.Toolbar(PaintedMask.color, a);
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.BeginVertical();
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Size", GUILayout.Width(48));
+		PaintedMask.brushSize = EditorGUILayout.IntSlider(PaintedMask.brushSize, 1, 100);
+		EditorGUILayout.EndHorizontal();
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Opacity", GUILayout.Width(48));
+		PaintedMask.opacity = EditorGUILayout.Slider(PaintedMask.opacity, 0f, 1f);
+		EditorGUILayout.EndHorizontal();
+		PaintedMask.color = GUILayout.Toggle(PaintedMask.color == 4, "Eraser", "Button") ? 4 : PaintedMask.color;
+		
+		if(GUILayout.Button("Clear Masks"))
 		{
-			if(m_brushes == null)
-			{ m_brushes = Brushes(); }
-			return m_brushes;
-		}
-	}
-
-	Projector brushProjector;
-	
-	public static void Initialize()
-	{
-		AlphaBrushWindow window = GetWindow<AlphaBrushWindow>(true, "AlphaBrush");
-		window.maxSize = new Vector2(350, 400);
-		window.minSize = new Vector2(350, 400);
-		window.Show();
-
-		window.OnOpen();
-	}
-
-	public void OnOpen()
-	{
-		//GameObject projectorObj = new GameObject("brushProjector");
-		//projectorObj.hideFlags = HideFlags.DontSave;
-		//brushProjector = projectorObj.AddComponent<Projector>();
-		//brushProjector.orthographic = true;
-		//brushProjector.material = Resources.Load<Material>("Editor/Materials/brushProjection");
-	}
-
-	Texture2D[] Brushes()
-	{
-		List<Texture2D> t = new List<Texture2D>();
-		t.AddRange(Resources.LoadAll<Texture2D>("Gizmos"));
-		t.AddRange(Resources.FindObjectsOfTypeAll<Texture2D>().Where( x => x.format == TextureFormat.Alpha8));
-		return t.ToArray();
-    }
-
-	void OnGUI()
-	{
-		GUILayout.BeginHorizontal();
-		color = GUILayout.Toolbar(color, a);
-		GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-		for (int i = 0; i < brushes.Length; i++)
-		{
-			if ((i) % xCount == 0)
+			if(EditorUtility.DisplayDialog("Clear Masks?", "This will clear all blendmaps!", "Clear", "Cancel"))
 			{
-				GUILayout.EndHorizontal();
-				GUILayout.BeginHorizontal();
-			}
-			if(GUILayout.Toggle(brush == i ,brushes[i], "Button", GUILayout.Width(54), GUILayout.Height(54)))
-			{
-				brush = i;
-				//brushProjector.material.SetTexture("_MainTex", brushes[i]);
+				mask.ClearMasks();
 			}
 		}
-		GUILayout.EndHorizontal();
-	}
 
-	void OnPostprocessTexture()
-	{
-		m_brushes = Brushes(); //update brushes if a texture is imported
-	}
-
-	void OnSceneGUI()
-	{
-		Event e = Event.current;
-		RaycastHit rayHit;
-		if(Physics.Raycast(Camera.current.ViewportPointToRay(e.mousePosition), out rayHit))
-		{
-			//brushProjector.gameObject.transform.position = new Vector3(rayHit.point.x, rayHit.point.y, brushProjector.gameObject.transform.position.z);
-			List<Vector3> points = new List<Vector3>();
-			for (int i = 0; i < 100; i++)
-			{
-				points.Add(rayHit.point -Vector3.forward*3 + Vector3.right * Mathf.Cos(i/ 25) + Vector3.up * Mathf.Sin(i/25));
-			}
-			Debug.Log("yay?");
-			Handles.DrawAAPolyLine(points.ToArray());
-        }
-	}
-	
-	// Window has been selected
-	void OnFocus()
-	{
-		// Remove delegate listener if it has previously
-		// been assigned.
-		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
-		// Add (or re-add) the delegate.
-		SceneView.onSceneGUIDelegate += this.OnSceneGUI;
-	}
-
-	void OnDestroy()
-	{
-		// When the window is destroyed, remove the delegate
-		// so that it will no longer do any drawing.
-		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
-	}
-
-	void OnSceneGUI(SceneView sceneView)
-	{
-		// Do your drawing here using Handles.
-		Handles.BeginGUI();
-		// Do your drawing here using GUI.
-		Handles.EndGUI();
+		EditorGUILayout.EndVertical();
+		return EditorGUI.EndChangeCheck();
 	}
 }
--- Assets/PaintedMask.cs
+++ Assets/PaintedMask.cs
@@ -1,35 +1,143 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[ExecuteInEditMode]
 public class PaintedMask : MonoBehaviour
 {
+	[SerializeField]
+	private Texture2D m_maskTex;
+	public Texture2D maskTex
+	{
+		get
+		{
+			if(m_maskTex == null)
+			{
+				m_maskTex = new Texture2D(2, 2, TextureFormat.RGBA4444, true);
+				m_maskTex.wrapMode = TextureWrapMode.Clamp;
+				m_maskTex.filterMode = FilterMode.Trilinear;
+				UpdateTextureSize();
+			}
+			return m_maskTex;
+		}
+		set { m_maskTex = value; }
+	}
 
-	Texture2D maskTex;
-	
+	[SerializeField]
+	public MaterialPropertyBlock m_propBlock;
+
+	SpriteRenderer m_renderer;
+	public SpriteRenderer spriteRenderer
+	{
+		get
+		{
+			if(m_renderer == null)
+			{
+				m_renderer = GetComponent<SpriteRenderer>();
+			}
+			return m_renderer;
+		}
+	}
+
+	[HideInInspector]
 	public bool isPainting;
 
-	public int resolutionMultiplier = 2;
+	public int resolutionMultiplier = 1;
+	
+	private static Color[] colors = { new Color(1, 0, 0, 0), new Color(0, 1, 0, 0), new Color(0, 0, 1, 0), new Color(0, 0, 0, 1), new Color(0, 0, 0, 0) };
+
+	public static int color;
+	public static int brushSize = 3;
+	public static float opacity = 1f;
+
+	public Color GetColor(int i)
+	{
+		return colors[i] * opacity;
+	}
+
+	private Color blank { get { return colors[4]; } }
+
+	void OnEnable()
+	{
+		m_propBlock = new MaterialPropertyBlock();
+		m_propBlock.SetTexture("_BlendMask", m_maskTex);
+		spriteRenderer.SetPropertyBlock(m_propBlock);
+		spriteRenderer.sprite.texture.wrapMode = TextureWrapMode.Repeat;
+		spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+		spriteRenderer.receiveShadows = true;
+	}
 
 	void OnValidate()
 	{
-		UpdateTextureSize();
+		resolutionMultiplier = Mathf.Max(1, resolutionMultiplier);
 	}
 
-	void UpdateTextureSize()
+	public void UpdateTextureSize()
 	{
-		maskTex = new Texture2D(
-			resolutionMultiplier * Mathf.RoundToInt(transform.localScale.x),
-			resolutionMultiplier * Mathf.RoundToInt(transform.localScale.y));
-		maskTex.wrapMode = TextureWrapMode.Clamp;
-		GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_BlendMask", maskTex);
+		m_maskTex.Resize(
+			resolutionMultiplier * Mathf.RoundToInt(transform.localScale.x*spriteRenderer.sprite.bounds.size.x),
+			resolutionMultiplier * Mathf.RoundToInt(transform.localScale.y*spriteRenderer.sprite.bounds.size.y));
+		m_maskTex.Apply();
+		ClearMasks();
+		m_propBlock.Clear();
+		m_propBlock.SetTexture("_BlendMask", m_maskTex);
+		m_renderer.SetPropertyBlock(m_propBlock);
+	}
+	
+	public void Paint(Vector2 pos)
+	{
+		Vector2 size = new Vector2(
+			Mathf.RoundToInt(transform.localScale.x * spriteRenderer.sprite.bounds.size.x),
+			Mathf.RoundToInt(transform.localScale.y * spriteRenderer.sprite.bounds.size.y));
+		Vector2 texPos = ( pos - (Vector2)transform.position + size * 0.5f) * resolutionMultiplier;
+		Circle(Mathf.RoundToInt(texPos.x), Mathf.RoundToInt(texPos.y), brushSize, GetColor(color));
+		maskTex.Apply();
+		m_propBlock.Clear();
+		m_propBlock.SetTexture("_BlendMask", m_maskTex);
+		m_renderer.SetPropertyBlock(m_propBlock);
+	}
+	
+	void Circle(int cx, int cy, int r, Color col)
+	{
+		int x, y, px, nx, py, ny, d;
 
+		for(x = 0; x <= r; x++)
+		{
+			d = (int)Mathf.Ceil(Mathf.Sqrt(r * r - x * x));
+			for(y = 0; y <= d; y++)
+			{
+				px = cx + x;
+				nx = cx - x;
+				py = cy + y;
+				ny = cy - y;
+
+				maskTex.SetPixel(px, py, col);
+				maskTex.SetPixel(nx, py, col);
+				maskTex.SetPixel(px, ny, col);
+				maskTex.SetPixel(nx, ny, col);
+
+			}
+		}
+	}
+
+	public void Erease(Vector2 pos)
+	{
+		int c = color;
+		color = 4;
+		Paint(pos);
+		color = c;
+	}
+	public void ClearMasks()
+	{
 		for(int x = 0; x < maskTex.width; x++)
 		{
 			for(int y = 0; y < maskTex.height; y++)
 			{
-				maskTex.SetPixel(x, y, Color.red * ( (float)x / maskTex.width ) + Color.green * ( (float)y / maskTex.height ) - new Color(0, 0, 0, 1));
+				maskTex.SetPixel(x, y, blank);
 			}
 		}
 		maskTex.Apply();
+		m_propBlock.Clear();
+		m_propBlock.SetTexture("_BlendMask", m_maskTex);
+		m_renderer.SetPropertyBlock(m_propBlock);
 	}
+	
 }
