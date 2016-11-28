079c44128acad29aead5b5f55afa8bb455637739
Halvor Bakken Smedås <stektpotet@gmail.com>
2016-11-28 19:47:21 +0100

    started working on levelsaving

---
+++ Assets/Game/Scripts/SaveLoadManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public static class SaveLoadManager
+{
+	public static FileStream CreateFileStream(string fileName)
+	{
+		return = new FileStream(Application.persistentDataPath + "/"+fileName + ".hornes", FileMode.Create);
+	}
+	public static void SavePlayerStats(PlayerStats stats)
+	{
+		
+    }
+
+	[System.Serializable]
+	public class PlayerStatsData
+	{
+		public string playerName = "Hornes";
+		public int[] ints;
+
+		public void SaveData()
+		{
+
+		}
+	}
+}
