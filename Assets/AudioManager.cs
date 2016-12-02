3f2a31a96f0e9e28f707094c83567007a966811e
Halvor <stektpotet@gmail.com>
2016-12-02 01:10:15 +0100

    Moved Audio files, added volume control, and fixed animation-error

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+	public AudioMixer mixer;
+
+	public void SetMasterVolume(float v)
+	{
+		mixer.SetFloat("masterVol", v);
+	}
+	public void SetMusicVolume(float v)
+	{
+		mixer.SetFloat("musicVol", v);
+	}
+	public void SetSFXVolume(float v)
+	{
+		mixer.SetFloat("sfxVol", v);
+	}
+
+}
