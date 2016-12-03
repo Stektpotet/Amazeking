eb3b49fa7be06e57a1067f70d14f81296eb496b7
Halvor Bakken Smedås <stektpotet@gmail.com>
2016-12-03 05:15:07 +0100

    more timer stuff

---
+++ Assets/StartTimer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTimer : MonoBehaviour
+{
+	public void EnableTimer()
+	{
+		GameManager.timed = true;
+	}
+	public void DisableTimer()
+	{
+		GameManager.timed = false;
+	}
+}
